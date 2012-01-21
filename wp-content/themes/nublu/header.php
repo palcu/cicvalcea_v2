8f98f48ef67a48820a60eccc69e43f6eae3a8c5d
Alex Palcuie <alex.palcuie@gmail.com>
2012-01-21 18:31:30 +0200

    finished facebook integration

--- wp-content/themes/nublu/header.php
+++ wp-content/themes/nublu/header.php
@@ -67,6 +67,17 @@ getTwitters('tweet', {
 <?php wp_head(); ?>
 </head>
 <body>
+
+<!-- Facebook SDK -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div class="wrapper">
 
   <div class="access">
